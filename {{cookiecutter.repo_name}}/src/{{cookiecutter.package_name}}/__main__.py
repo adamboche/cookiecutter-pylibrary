bb1b487bb4e4c56d9dd342859a645b041f0cc59d
adamboche <adamboche@gmail.com>
2019-02-28 15:36:35 -0800

    Fix __main__.py cli call.

--- {{cookiecutter.repo_name}}/src/{{cookiecutter.package_name}}/__main__.py
+++ {{cookiecutter.repo_name}}/src/{{cookiecutter.package_name}}/__main__.py
@@ -11,7 +11,7 @@ Why does this file exist, and why __main__? For more info, read:
 {%- if cookiecutter.command_line_interface == 'plain' %}
 import sys
 {% endif %}
-import {{cookiecutter.package_name}}.cli
+import {{cookiecutter.package_name}}.cli.cli()
 
 if __name__ == "__main__":
 {%- if cookiecutter.command_line_interface == 'plain' %}
