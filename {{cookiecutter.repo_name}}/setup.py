88d24f490cd56b03d4006f63f75ea188d386091c
adamboche <adamboche@gmail.com>
2019-04-30 16:54:46 -0700

    Set status alpha

--- {{cookiecutter.repo_name}}/setup.py
+++ {{cookiecutter.repo_name}}/setup.py
@@ -106,7 +106,7 @@ setup(
     zip_safe=False,
     classifiers=[
         # complete classifier list: http://pypi.python.org/pypi?%3Aaction=list_classifiers
-        'Development Status :: 5 - Production/Stable',
+        'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
 {%- if cookiecutter.license in ["BSD 2-Clause License", "BSD 3-Clause License"] %}
         'License :: OSI Approved :: BSD License',
