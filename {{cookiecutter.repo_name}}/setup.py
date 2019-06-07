bd1aad926315d366ca9f9bea0988023bca7828b2
adamboche <adamboche@gmail.com>
2019-06-07 22:55:35 +0000

    Replace pypi.python.org with pypi.org

--- {{cookiecutter.repo_name}}/setup.py
+++ {{cookiecutter.repo_name}}/setup.py
@@ -104,7 +104,7 @@ setup(
     include_package_data=True,
     zip_safe=False,
     classifiers=[
-        # complete classifier list: http://pypi.python.org/pypi?%3Aaction=list_classifiers
+        # complete classifier list: http://pypi.org/pypi?%3Aaction=list_classifiers
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
 {%- if cookiecutter.license in ["BSD 2-Clause License", "BSD 3-Clause License"] %}
